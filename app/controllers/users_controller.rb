6da2a2c6291e478f614606cdc33869927ed42347
Paula Murphy <paulam849@gmail.com>
2016-01-15 21:24:00 -0500

    added new page and form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,25 @@
 class UsersController < ApplicationController
 	def index
+		@users = User.all
+	end
+
+	def show
+		@user = User.find(params[:id])
+	end
+
+	def new
+		@user = User.new
+	end
+
+	def create
+	end
+
+	def edit
+	end
+
+	def update
+	end
+
+	def delete
 	end
 end
