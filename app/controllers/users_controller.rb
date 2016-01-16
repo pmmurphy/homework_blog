84cac2db7959c0787f3bd585898a13209d63b937
Paula Murphy <paulam849@gmail.com>
2016-01-16 01:07:06 -0500

    added edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,6 +12,14 @@ class UsersController < ApplicationController
 	end
 
 	def create
+		@user = User.create(params[:user])
+		if @user.save
+			flash[:notice] = "Your account was created"
+			redirect_to @user
+		else
+			flash[:notice] = "There was a problem"
+			redirect_to_new_user_path	
+		end
 	end
 
 	def edit
@@ -20,6 +28,12 @@ class UsersController < ApplicationController
 	def update
 	end
 
-	def delete
+	def destroy
+		@user = User.find(params[:id])
+		if @user.destroy
+		else
+
+		end
+		redirect_to users_path	
 	end
 end
